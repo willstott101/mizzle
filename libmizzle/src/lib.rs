31bd303cf436f37c5671ca7e6fd1c6d06fe18478
Will Stott <willstott101@gmail.com>
2022-10-11 18:13:08 +0100

    Link to inspiration

--- libmizzle/src/lib.rs
+++ libmizzle/src/lib.rs
@@ -6,6 +6,8 @@ pub fn add(left: usize, right: usize) -> usize {
 mod tests {
     use super::*;
 
+    https://github.com/willstott101/node-git-server/blob/auth-context/src/git.ts#L520
+
     #[test]
     fn it_works() {
         let result = add(2, 2);
