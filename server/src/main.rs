4a121a2d073d77ece1ac79d5edc51e5015ea094e
Will Stott <willstot101@gmail.com>
2023-12-01 23:52:00 +0000

    Figured out how to stream using pipes

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,17 +2,17 @@ use trillium_smol;
 use trillium::Conn;
 use simple_logger::SimpleLogger;
 use log::info;
-use form_urlencoded;
-use std::collections::HashMap;
+// use form_urlencoded;
+// use std::collections::HashMap;
+use gix_packetline::encode::{text_to_write, flush_to_write};
 
-const NAME: &str = "mizzle";
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     SimpleLogger::new().init().unwrap();
 
     // port 8080
-    trillium_smol::run(|mut conn: trillium::Conn| async move {
+    trillium_smol::run(|conn: trillium::Conn| async move {
         if conn.headers().get_str("Git-Protocol").unwrap_or("version=2") != "version=2" {
             println!("Only Git Protocol 2 is supported");
             return conn.with_status(trillium::Status::NotImplemented).with_body("Only Git Protocol 2 is supported").halt();
@@ -48,12 +48,26 @@ async fn serve_git_protocol_2(mut conn: trillium::Conn, repo_path: Box<str>, pro
     info!("REQUEST CONTENT TYPE {:?}", conn.headers().get_str("Content-Type"));
     info!("HTTP {} {} {}", conn.method(), conn.path(), conn.querystring());
     info!("GIT {} {}", repo_path, protocol_path);
-    info!("RESPONDING {}", format!("000eversion 2
-agent={}/{}
-0000", NAME, VERSION));
 
-    // Missing leading length i think
-    // use gix-packetline text_to_write
+    let (reader, mut writer) = piper::pipe(4096);
+    trillium_smol::spawn((|| async move {
+        // Copied from github
+        text_to_write(b"# service=git-upload-pack", &mut writer).await.expect("to write to output");
+        flush_to_write(&mut writer).await.expect("to write to output");
+
+        // Understood in the spec
+        text_to_write(b"version 2", &mut writer).await.expect("to write to output");
+        text_to_write(b"agent=mizzle/dev", &mut writer).await.expect("to write to output");
+
+        // Copied from github
+        text_to_write(b"ls-refs=unborn", &mut writer).await.expect("to write to output");
+        text_to_write(b"fetch=shallow wait-for-done filter", &mut writer).await.expect("to write to output");
+        text_to_write(b"server-option", &mut writer).await.expect("to write to output");
+        text_to_write(b"object-format=sha1", &mut writer).await.expect("to write to output");
+
+        // Understood in the spec
+        flush_to_write(&mut writer).await.expect("to write to output");
+    })());
 
     // 001e# service=git-upload-pack
     // 0000000eversion 2
@@ -64,7 +78,8 @@ agent={}/{}
     // 0017object-format=sha1
     // 0000
 
-    conn.with_status(trillium::Status::Ok).with_body(format!("000eversion 2\nagent={}/{}\n0000", NAME, VERSION))
+    conn.with_status(trillium::Status::Ok).with_body(trillium::Body::new_streaming(reader, None)).halt()
+    // conn.with_status(trillium::Status::Ok).with_body(format!("000eversion 2\nagent={}/{}\n0000", NAME, VERSION))
 
     // info!("BODY: {:#?}", conn.request_body_string().await);
 }
